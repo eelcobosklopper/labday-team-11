4e63d3da6828c99adc091f2131a891312878efb5
olafvanv <olafvanv@gmail.com>
2021-12-17 14:36:52 +0100

    speech-button + speech sample page

---
+++ src/components/speech-button.svelte
@@ -0,0 +1,45 @@
+<script>
+	import Button from './button.svelte';
+	import { createEventDispatcher, onMount } from 'svelte';
+
+	let recognition;
+
+	onMount(() => {
+		if ('webkitSpeechRecognition' in window) {
+			const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+			recognition = new SpeechRecognition();
+
+			// Set speech recognition options
+			recognition.continuous = false;
+			recognition.interimResults = false;
+			recognition.lang = 'nl-NL';
+
+			recognition.onresult = (res) => {
+				const result = res.results[0][0].transcript;
+				dispatch('result', {
+					text: result
+				});
+			};
+
+			recognition.onend = () => {
+				active = false;
+				dispatch('stop');
+			};
+			recognition.onstart = () => {
+				active = true;
+				dispatch('start');
+			};
+		} else {
+			console.warn('Speech recognition not available')
+		}
+	});
+
+	const dispatch = createEventDispatcher();
+	let active = false;
+
+	function toggleSpeech() {
+		active ? recognition.stop() : recognition.start();
+	}
+</script>
+
+<Button click={toggleSpeech} text={active ? 'Stop' : 'Start'} />
