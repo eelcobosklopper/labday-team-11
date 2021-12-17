861f237093a1d3dcfe973764f15d0a0e1508d30b
Eelco <eelco.bosklopper@capgemini.com>
2021-12-17 11:53:12 +0100

    Added some svelte examples

---
+++ src/components/button.svelte
@@ -0,0 +1,6 @@
+<script>
+	export let click;
+	export let text;
+</script>
+
+<button on:click={click}>{text}</button>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,41 @@
+<script>
+	import Button from '../components/button.svelte';
+	let visible = false;
+
+	const cats = [
+		{ id: 'J---aiyznGQ', name: 'Keyboard Cat' },
+		{ id: 'z_AbfPXTKms', name: 'Maru' },
+		{ id: 'OUtn3pvWmpg', name: 'Henri The Existential Cat' }
+	];
+
+	function toggleMe() {
+		visible = !visible;
+	}
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<ul>
+	{#each cats as cat}
+		<li>
+			<a target="_blank" href="https://www.youtube.com/watch?v={cat.id}">
+				{cat.name}
+			</a>
+		</li>
+	{/each}
+</ul>
+
+<Button click={toggleMe} text="Click me" />
+
+{#if visible}
+	<p>Hello!</p>
+{/if}
+
+<style>
+	p {
+		color: purple;
+		font-family: 'Comic Sans MS', cursive;
+		font-size: 2em;
+	}
+</style>
