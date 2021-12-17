aa4c0bdfb09cfd92db8618285f21f164b5c66f81
Thijs Voncken <thijs.voncken@capgemini.com>
2021-12-17 13:07:38 +0100

    Basic audio player.

---
+++ src/effects/audio/AudioPlayer.js
@@ -0,0 +1,42 @@
+export class AudioPlayer {
+    constructor() {
+        this._initialized = false;
+    }
+
+    init() {
+        if (this._initialized) { return; }
+
+        this._buffers = new Map();
+        this._context = new AudioContext();
+        this._initialized = true;
+    }
+
+    async load(key, url) {
+        if (!this._initialized) { throw new Error('AudioPlayer not initialized.'); };
+        
+        return new Promise((resolve) => {
+            const request = new XMLHttpRequest();
+            request.open('GET', url, true);
+            request.responseType = 'arraybuffer';
+
+            request.onload = () => {
+                this._context.decodeAudioData(
+                    request.response, 
+                    (buffer) => {
+                        this._buffers.set(key, buffer);
+                        resolve(this._buffers.get(key));
+                    });
+            };
+            request.send();
+        });
+    }
+
+    async play(key) {
+        if (!this._initialized) { throw new Error('AudioPlayer not initialized!'); };
+
+        const source = this._context.createBufferSource();
+        source.buffer = this._buffers.get(key);
+        source.connect(this._context.destination);
+        source.start(0);
+    }
+}
